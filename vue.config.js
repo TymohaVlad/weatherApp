b716a5412ab9ff45a0dda39286c2cb7f526259f5
Vladyslav Tymoshenko <vlados230761@gmail.com>
2023-01-18 21:46:22 +0200

    added changes

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+export default defineConfig({
+  bace:'/deploying-vite-project-example/',
+  plugins: [vue()]
+})
