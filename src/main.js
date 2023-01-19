cfc0098d20e4e1750ee86213130019a8b7369295
Vladyslav Tymoshenko <vlados230761@gmail.com>
2023-01-19 10:35:15 +0200

    fixed resources path

--- src/main.js
+++ src/main.js
@@ -1,26 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createWebHistory, createRouter } from 'vue-router'
 
 createApp(App).mount('#app')
-
-
-const routes = [
-	{
-		path: '/',
-		name: 'products',
-		component: Products
-	},
-    {
-        path: '/billPage',
-        name: 'BillPage',
-        component: BillPage
-    }
-];
-
-const router = createRouter({
-	history: createWebHistory(),
-	routes,
-});
-
-export default router;
